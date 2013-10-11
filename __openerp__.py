c11d2ddb672c4a8f4fa30f92f45fd494ba3577cb
Emad Shaaban <emad.shaaban92@gmail.com>
2013-10-11 17:30:33 +0200

    Create openerp module descriptor file

---
+++ __openerp__.py
@@ -0,0 +1,23 @@
+{
+    'name' : 'Maintenance',
+    'version' : '0.1',
+    'author' : 'Emad Shaaban',
+    'sequence': 110,
+    'description' : """
+Maintenance
+==================================
+Bla Bla Bla
+
+""",
+    'depends' : [
+        'base',
+        'mail',
+        'board',
+        'hr',
+    ],
+
+    
+    'installable' : True,
+    'application' : True,
+    'auto_install': False,
+}
