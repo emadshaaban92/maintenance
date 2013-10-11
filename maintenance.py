ddbb199b7983b4c8a5577d61eba816662cc7d498
Emad Shaaban <emad.shaaban92@gmail.com>
2013-10-11 17:45:52 +0200

    Add Asset Type Model

--- maintenance.py
+++ maintenance.py
@@ -1,2 +1,9 @@
 from openerp.osv import fields, osv
 
+class maintenance_asset_type(osv.Model):
+    _name = 'maintenance.asset.type'
+    _columns = {
+        'name': fields.char('Name', required=True),
+    }
+
+maintenance_asset_type()
